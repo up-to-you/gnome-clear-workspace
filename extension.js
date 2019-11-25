4a8759ae62086932f90a7f5bd0f5db13d32d80c5
up-to-you <sabirovdev@gmail.com>
2019-11-25 17:21:24 +0300

    gnome 3.34 support

--- extension.js
+++ extension.js
@@ -3,22 +3,15 @@ const Main = imports.ui.main;
 
 function init() {
 	Main.layoutManager.connect('startup-complete', Lang.bind(this, function () { 
-			Main.overview._dash.actor.hide();
-		
-// 		        Gnome 3.30 failure
-// 			Main.overview._controls._thumbnailsBox.actor.destroy_all_children();
-// 			Main.overview._controls._thumbnailsBox.actor.destroy();
-
-// 			Main.overview._controls._thumbnailsSlider.actor.destroy_all_children();
-// 			Main.overview._controls._thumbnailsSlider.actor.destroy();
+			Main.overview._dash.hide();
 		})
 	);
 }
 
 function enable() {
-	Main.overview._dash.actor.hide();
+	Main.overview._dash.hide();
 }
 
 function disable() {
-	Main.overview._dash.actor.show();
+	Main.overview._dash.show();
 }
